5550d0bcf845501f79ad117e9e54d3a04badccdd
ChoonsikJS <junsic98@naver.com>
2026-09-10 13:35:00 +0900

    [Passed] 반복 출력하기 2 (128ms, 11MB)

---
+++ trail2/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    static void 재귀(int n){
+        if(n==0) return;
+        재귀(n-1);
+        System.out.println("HelloWorld");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        재귀(n);
+        // Please write your code here.
+    }
+}
