bdd8664fea33e1633eac05b497c959607495b686
ChoonsikJS <junsic98@naver.com>
2026-09-16 14:42:20 +0900

    [Passed] 연속되는 수 2 (185ms, 12MB)

---
+++ trail2/연속되는 수 2/continuous-number2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int cnt = 0;
+        int max = 1;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                cnt++;
+                max = Math.max(max, cnt);
+            } else
+                cnt = 1;
+        }
+        System.out.println(max);
+        // Please write your code here.
+    }
+}
