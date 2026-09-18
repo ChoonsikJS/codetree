2484231b14a8bdc36fbcc6c3370d383abaf8776f
ChoonsikJS <junsic98@naver.com>
2026-09-18 15:46:07 +0900

    [Passed] 숨은 단어 찾기 2 (153ms, 10MB)

--- trail2/숨은 단어 찾기 2/find-hidden-words-2.java
+++ trail2/숨은 단어 찾기 2/find-hidden-words-2.java
@@ -2,13 +2,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static int n, m;
     static final int[] dr = { -1, 1, 0, 0, -1, -1, 1, 1 }; // 상하좌우 좌우 좌우
     static final int[] dc = { 0, 0, -1, 1, -1, 1, -1, 1 };
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
         char[][] arr = new char[n][m];
         String str;
         for (int i = 0; i < n; i++) {
@@ -17,24 +18,25 @@ public class Main {
         }
 
         int ans = 0;
-        // L을 찾고 8방향 2칸 탐색
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 'L') {
                     for (int d = 0; d < 8; d++) {
-                        if (i + dr[d] >= 0 && j + dc[d] >= 0 &&
-                                i + dr[d] < n && j + dc[d] < m &&
-                                arr[i + dr[d]][j + dc[d]] == 'E') {
-                            if (i + dr[d] + dr[d] >= 0 && j + dc[d] + dc[d] >= 0 &&
-                                    i + dr[d] + dr[d] < n && j + dc[d] + dc[d] < m &&
-                                    arr[i + dr[d] + dr[d]][j + dc[d] + dc[d]] == 'E') {
-                                ans++;
-                            }
+                        int r1 = i + dr[d];
+                        int r2 = i + (dr[d] * 2);
+                        int c1 = j + dc[d];
+                        int c2 = j + (dc[d] * 2);
+                        if (range(r1, c1) && range(r2, c2) &&
+                            arr[r1][c1] == 'E' && arr[r2][c2] == 'E') {
+                            ans++;
                         }
                     }
                 }
             }
         }
-        System.out.println(ans);
+    System.out.println(ans);
+}
+    static boolean range(int r, int c) {
+        return r >= 0 && c >= 0 && r < n && c < m;
     }
 }
